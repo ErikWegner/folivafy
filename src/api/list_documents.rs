ca5d56877e8b4664aef3505c98aefd78a0b47918
Erik Wegner <e_wegner@web.de>
2023-04-23 17:31:38 +0000

    Limit list to title

--- src/api/list_documents.rs
+++ src/api/list_documents.rs
@@ -6,12 +6,23 @@ use axum_macros::debug_handler;
 use entity::collection_document::Entity as Documents;
 use jwt_authorizer::JwtClaims;
 use openapi::models::CollectionItemsList;
-use sea_orm::{ColumnTrait, EntityTrait, PaginatorTrait, QueryFilter, QueryOrder, QuerySelect};
+use sea_orm::{
+    prelude::Uuid, ColumnTrait, EntityTrait, FromQueryResult, PaginatorTrait, QueryFilter,
+    QueryOrder, QuerySelect,
+};
+use sea_query::Expr;
+use serde_json::json;
 use tracing::warn;
 
 use crate::{api::auth::User, axumext::extractors::ValidatedQueryParams};
 
 use super::{db::get_collection_by_name, types::Pagination, ApiContext, ApiErrors};
+
+#[derive(FromQueryResult)]
+struct DocumentIdAndTitleQueryResult {
+    id: Uuid,
+    title: String,
+}
 
 #[debug_handler]
 pub(crate) async fn api_list_document(
@@ -44,10 +55,14 @@ pub(crate) async fn api_list_document(
         .await
         .map_err(ApiErrors::from)
         .map(|t| u32::try_from(t).unwrap_or_default())?;
-    let items = basefind
+    let items: Vec<DocumentIdAndTitleQueryResult> = basefind
+        .select_only()
+        .columns([entity::collection_document::Column::Id])
+        .column_as(Expr::cust("f->>'title'"), "title")
         .order_by_asc(entity::collection_document::Column::Id)
         .limit(Some(pagination.limit().into()))
         .offset(Some(pagination.offset().into()))
+        .into_model::<DocumentIdAndTitleQueryResult>()
         .all(&ctx.db)
         .await
         .map_err(ApiErrors::from)?;
@@ -59,7 +74,7 @@ pub(crate) async fn api_list_document(
             .iter()
             .map(|dbitem| openapi::models::CollectionItem {
                 id: dbitem.id,
-                f: dbitem.f.clone(),
+                f: json!({"title": dbitem.title}),
             })
             .collect(),
     }))
