8c02a6bc2d8fffef6b9efd6d82d1c1c17001209c
Erik Wegner <E_Wegner@web.de>
2023-06-23 21:05:16 +0200

    Events migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_basic;
+mod m20230623_190444_events;
 
 pub struct Migrator;
 pub use m20220101_000001_basic::CollectionDocument;
@@ -8,6 +9,9 @@ pub use m20220101_000001_basic::CollectionDocument;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_basic::Migration)]
+        vec![
+            Box::new(m20220101_000001_basic::Migration),
+            Box::new(m20230623_190444_events::Migration),
+        ]
     }
 }
