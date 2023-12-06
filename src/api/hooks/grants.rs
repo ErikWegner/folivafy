97420daa098d8f1cde0cc23077388e47c8f3ab25
Erik Wegner <e_wegner@web.de>
2023-12-06 20:58:31 +0100

    WIP grants

--- src/api/hooks/grants.rs
+++ src/api/hooks/grants.rs
@@ -1,20 +1,38 @@
 use async_trait::async_trait;
 
-use crate::api::dto;
+use crate::api::{data_service::DataService, dto};
 
 use super::HookResult;
+
 pub struct HookUserGrantContext {
     user: dto::UserWithRoles,
+    data_service: std::sync::Arc<dyn DataService>,
 }
 
 impl HookUserGrantContext {
-    pub fn new(user: dto::UserWithRoles) -> Self {
-        Self { user }
+    pub fn new(user: dto::UserWithRoles, data_service: std::sync::Arc<dyn DataService>) -> Self {
+        Self { user, data_service }
     }
 }
 
 pub struct HookDocumentGrantContext {
+    collection: dto::Collection,
     document: dto::CollectionDocument,
+    data_service: std::sync::Arc<dyn DataService>,
+}
+
+impl HookDocumentGrantContext {
+    pub fn new(
+        collection: dto::Collection,
+        document: dto::CollectionDocument,
+        data_service: std::sync::Arc<dyn DataService>,
+    ) -> Self {
+        Self {
+            collection,
+            document,
+            data_service,
+        }
+    }
 }
 
 #[async_trait]
